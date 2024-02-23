ca9506e0834e6bf95fcc1b3eed2f785c147c8c6f
Osipovone <xopow52@gmail.com>
2024-02-23 17:41:20 +0300

    add patch and filter (user_email)

--- Virt_perevals/Pereval/views.py
+++ Virt_perevals/Pereval/views.py
@@ -31,12 +31,13 @@ class ImageViewset(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
 
 
-class PerevalViewset(viewsets.ModelViewSet):
+class PerevalCreateViewset(viewsets.ModelViewSet):
 
     queryset = PerevalAdded.objects.filter(id=0)
     serializer_class = PerevalSerializer
 
     def create(self, request, *args, **kwargs):
+
         serializer = PerevalSerializer(data=request.data)
         response_data = {}
 
@@ -65,11 +66,52 @@ class PerevalViewset(viewsets.ModelViewSet):
         return Response(response_data)
 
 
+class PerevalUpdateViewset(viewsets.ModelViewSet):
+
+    queryset = PerevalAdded.objects.all().order_by('id')
+    serializer_class = PerevalSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+
+        pereval = self.get_object()
+        serializer = PerevalSerializer(pereval, data=request.data, partial=True)
+
+        if request.data['status'] == 'New':
+            if serializer.is_valid():
+                serializer.save()
+                response_data = {
+                    'status': 1,
+                    'message': 'Данные успешно изменены',
+                }
+                return Response(response_data, status=status.HTTP_200_OK)
+            else:
+                response_data = {
+                    'status': 0,
+                    'message': 'Данные о пользователе нельзя изменять',
+                }
+                return Response(response_data, status=status.HTTP_403_FORBIDDEN)
+        else:
+            response_data = {
+                'status': 0,
+                'message': 'Изменение отклонено. Данные проходят проверку модератором',
+            }
+            return Response(response_data, status=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS)
+
+
 class PerevalListView(APIView):
 
     def get(self, request):
 
-        perevals = PerevalAdded.objects.all()
+        perevals = PerevalAdded.objects.all().order_by('id')
         serializer = PerevalListSerializer(perevals, many=True)
 
         return Response(serializer.data)
+
+
+    class PerevalUserListViewset(APIView):
+
+        def get(self, request, email):
+            perevals = PerevalAdded.objects.filter(user__email=email).order_by('id')
+            serializer = PerevalListSerializer(perevals, many=True)
+
+            return Response(serializer.data)
