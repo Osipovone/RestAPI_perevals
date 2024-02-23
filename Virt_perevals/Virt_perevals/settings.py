4f2c96748e6b6b6a02e5f32de1a76f6df95e86f4
Osipovone <xopow52@gmail.com>
2024-02-23 17:50:00 +0300

    update DB postgres settings

--- Virt_perevals/Virt_perevals/settings.py
+++ Virt_perevals/Virt_perevals/settings.py
@@ -83,19 +83,19 @@ WSGI_APPLICATION = 'Virt_perevals.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'rest_pereval',
-    #     'USER': os.getenv('FSTR_DB_LOGIN'),
-    #     'PASSWORD': os.getenv('FSTR_DB_PASS'),
-    #     'HOST': os.getenv('FSTR_DB_HOST'),
-    #     'PORT': os.getenv('FSTR_DB_PORT'),
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rest_pereval',
+        'USER': os.getenv('FSTR_DB_LOGIN'),
+        'PASSWORD': os.getenv('FSTR_DB_PASS'),
+        'HOST': os.getenv('FSTR_DB_HOST'),
+        'PORT': os.getenv('FSTR_DB_PORT'),
+    }
 }
 
 
